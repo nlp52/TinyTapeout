63ef9c1061b271fe46cd16e5d3f65036190186ef
nlp52 <132784612+nlp52@users.noreply.github.com>
2024-09-24 21:32:52 -0400

    now wiring up to pairtripledetector

--- test/tb.v
+++ test/tb.v
@@ -24,7 +24,7 @@ module tb ();
   wire [7:0] uio_oe;
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+   tt_um_nlp52_PairTripleDetector #(.MAX_COUNT(10000) tt_um_nlp52_PairTripleDetector(
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
