fa1d660684157dc80f12e8cf0628477adb281b0d
nlp52 <nlp52@cornell.edu>
2024-10-01 20:46:02 -0400

    removing max count

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-   tt_um_nlp52_PairTripleDetector #(.MAX_COUNT(10000)) tt_um_nlp52_PairTripleDetector(
+   tt_um_nlp52_PairTripleDetector(
 
       // Include power ports for the Gate Level test:
     `ifdef GL_TEST
